cdf6e5c75d1242908b8129781b3325279ba7bb30
Prince Odia <princeodia@gmail.com>
2022-04-28 03:00:45 +0100

    Update project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import Fastify from 'fastify'
-import routes from './routes'
+import routes from './routes.js'
 const fastify = Fastify({ logger: true })
 
 fastify.register(routes)
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 8080)
+    fastify.listen(process.env.PORT || 8080)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
